c76ca85db0f0513b5c9b8572325cb31f98c93c13
rudra_pandey <pandeyrudransh16@gmail.com>
2026-09-02 20:15:26 +0530

    Calculating sum in java

---
+++ Sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Sum {
+    public static void main(String []args){
+        Scanner Sc = new Scanner(System.in);
+        int a = Sc.nextInt();
+        int b = Sc.nextInt();
+        int Sum = a+b;
+        System.out.println(Sum);
+    }
+    
+}
