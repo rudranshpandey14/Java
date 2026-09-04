571fc97a35d5038e8d000ed74cf98ba2d5e9b1f9
rudra_pandey <pandeyrudransh16@gmail.com>
2026-09-04 17:37:34 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args){
+    String name = "Tony Stark";
+    int age = 48;
+    double price = 25.25;
+    int a = 25;
+    int b = 10;
+    System.out.print(name);
+    }
+    
+}
